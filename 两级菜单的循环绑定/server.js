091ab12cfaa7621b4227c5feefb7e878839d4e31
wuxinkai <275648208@qq.com>
2016-09-22 15:54:17 +0800

    动态两级菜单循环绑定

---
+++ 两级菜单的循环绑定/server.js
@@ -0,0 +1,26 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+var server = http.createServer(function (request, response) {
+
+    var data = fs.readFileSync("./data.json", "utf8");
+    response.write(data)
+
+
+
+
+
+
+});
+server.listen(8888);
+
+
+
+
+
+
+
+
+
+
